412ddce64f1a9e494ce94954244a00ea36523983
silenceshell <me@ieevee.com>
2019-02-19 14:21:31 +0800

    add go mod

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-GOPATH=/home/bottle/Code/Go/ GO111MODULE=off go build
+# for go before 1.11
+#GOPATH=/home/bottle/Code/Go/ GO111MODULE=off go build
+
+# for go after 1.11
+GOPROXY="https://gocenter.io" GO111MODULE=on go build
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/silenceshell/kube-role-finder
+
+require (
+	k8s.io/api v0.0.0-20181204000039-89a74a8d264d
+	k8s.io/apimachinery v0.0.0-20181127025237-2b1284ed4c93
+	k8s.io/client-go v10.0.0+incompatible
+)
